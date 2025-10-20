613d16836b48ee2324b0481e88ee6c3a52bb8262
Bryan <bryanmartel.po@gmial.com>
2025-10-20 15:19:32 +0000

    task 6-2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 printf("Size of of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of of a int: %ld byte(s)\n", sizeof(int));
+printf("Size of of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of of a float: %ld byte(s)\n", sizeof(float));
