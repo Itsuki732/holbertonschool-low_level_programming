1efdbf58a494bdd7c970287cfd7ec148fff27e4e
Bryan <bryanmartel.po@gmial.com>
2025-10-20 15:18:09 +0000

    task 6-1

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*prints the size of various types on the computer it is compiled and run on
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of of a int: %ld byte(s)\n", sizeof(int));
+printf("Size of of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of of a float: %ld byte(s)\n", sizeof(float));
+return (0);
+}
