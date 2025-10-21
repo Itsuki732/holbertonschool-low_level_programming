55ce81fa695edba9eedf2f0996623d78d2c25414
Bryan <bryanmartel.po@gmial.com>
2025-10-21 08:53:12 +0000

    task 1-1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - Entry point
+*assign a random number to the variable n each time it is executed.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 5)
+{
+printf("is %d and is greater than 5\n", n);
+}
+else if (n == 0)
+{
+printf("is %d and is 0\n", n);
+}
+else
+{
+printf("is %d and is less than 6 and not 0\n", n);
+}
+return (0);
+}
