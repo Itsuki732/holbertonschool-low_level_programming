b3adff36b9b704081bb9ccbda704600817c9d1db
Bryan <bryanmartel.po@gmial.com>
2025-10-22 14:03:15 +0000

    task 4 test

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha - Entry point
+* @c: character to check
+*
+* Return: 1 if c is lowercase or uppercase, 0 otherwise
+*
+*/
+
+int _isalpha(int c)
+{
+if (c >=65 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
