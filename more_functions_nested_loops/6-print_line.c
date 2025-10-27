39811a1c2227cc08ca92e01a75fa31dc2fcffc71
Bryan <bryanmartel.po@gmial.com>
2025-10-27 11:55:30 +0000

    task 6 start

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_line - entry point
+*@n:nombre de caractère à afficher
+*
+*Écrivez une fonction qui trace une ligne droite dans le terminal.
+*
+*Prototype : void print_line(int n);
+*Vous ne pouvez utiliser que la fonction _putchar pour afficher.
+*Où n est le nombre de fois où le caractère _ doit être affiché.
+*La ligne doit se terminer par un \n.
+*Si n est inférieur ou égal à 0, la fonction doit uniquement afficher \n.
+*
+*/
+
+
+void print_line(int n)
+{
+int i;
+
+while (i < n)
+{
+	_putchar('_');
+	i++;
+}
+_putchar('\n');
+}
