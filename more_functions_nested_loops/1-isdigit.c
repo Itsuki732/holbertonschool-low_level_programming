9343d78b371658707e7a4c1c2e5d3578e7474482
Bryan <bryanmartel.po@gmial.com>
2025-10-27 09:08:21 +0000

    t

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isdigit - Entry point
+* @c: character to check
+*
+* Return: 1*0 if c is lowercase, 1 otherwise
+*
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		{
+		return (0);
+	}
+	return (0);
+}
