a62f955cf6e9ef904f164fab9b781fd5ba49579f
Bryan <bryanmartel.po@gmial.com>
2025-11-04 14:09:59 +0000

    task 1 modifi code

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while(n)
+unsigned int i = 0;
+
+	while(i < n)
 	{
-		src[n] = *dest;
-		n--;
+		dest[i] = src[i];
+		i++;
 	}
 
 return(dest);
