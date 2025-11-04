e347eebfedc675902615bde51b5c2f112479666d
Bryan <bryanmartel.po@gmial.com>
2025-11-04 14:15:10 +0000

    add doc

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+*_memcpy - main entry
+*@dest: target
+*@src: constant byte
+*@n: number of byte
+*
+*ecrire un fonction qui copie l'espace memoire
+*
+*Return: returns new value of target
+*/
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i = 0;
