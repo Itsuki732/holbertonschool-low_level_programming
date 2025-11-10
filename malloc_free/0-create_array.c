0425a014d41eacb2016cc4a104654ce14baf6711
Bryan <bryanmartel.po@gmial.com>
2025-11-10 10:28:24 +0000

    task 0 betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+* create_array - creates an array of char
+* @size: The size of the array
+* @c: The char to print
+* Return: a pointer to the array
+*/
+
 char *create_array(unsigned int size, char c)
 {
 	char *a;
