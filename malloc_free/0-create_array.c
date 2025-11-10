1d12e2b1ea4f4ebb864c1777d386315e74fb3d54
Bryan <bryanmartel.po@gmial.com>
2025-11-10 10:17:16 +0000

    task 0 add code

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	a = (char *)malloc((sizeof(char) * size));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+return (a);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#ifndef MYHEADER_H
+#define MYHAEDER_H
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
