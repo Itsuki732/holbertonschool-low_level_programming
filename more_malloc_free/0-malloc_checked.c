10e9908ba46ceca816d70a89fc7857465da4b7d5
Bryan <bryanmartel.po@gmial.com>
2025-11-12 09:22:26 +0000

    change betty

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+*malloc_checked - allocates memory using malloc
+*@b: The size of the memory to allocate
+*Return: a pointer to the allocated space
+*/
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
+
 	if (ptr == NULL)
 	{
 		exit(98);
