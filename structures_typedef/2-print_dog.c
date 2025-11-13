84ff7be08f4355d4111d11f784ff1fdea6cce548
Bryan <bryanmartel.po@gmial.com>
2025-11-13 15:50:45 +0000

    change age NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 
 	if (d->age <= 0.0)
-		printf("Age: (nil)\n");
+		printf("Age: 0.000000\n");
 	else
 		printf("Age: %f\n", d->age);
 
