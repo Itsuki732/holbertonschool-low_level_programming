7de0875f09fd2bd2fab45cfbd9acfcfb12d1d0c5
Bryan <bryanmartel.po@gmial.com>
2025-11-17 13:13:36 +0000

    add files

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #include <stddef.h>
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef Funtion_pointers_h
+#define Funtion_pointers_h
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
