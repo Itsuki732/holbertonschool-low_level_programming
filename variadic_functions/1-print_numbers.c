405ee12b6f1e055aa83e1dc571c6678c3428956e
Bryan <bryanmartel.po@gmial.com>
2025-11-19 10:50:47 +0000

    fix documantation betty (add blank ligne, add commentaire, supr space)

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,22 +2,34 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+* print_numbers - print all numbers in a list
+*
+* @separator: Use to separate numbers
+* @n: Number of the list
+*/
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int i;
 	va_list ap;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		int number = va_arg(ap, int);
-		printf ("%d",number);
+
+		printf("%d", number);
+
 		if (i < n - 1)
+		{
 			printf("%s", separator);
+		}
+
 	}
 
-	printf ("\n");
+	printf("\n");
 
-va_end (ap);
+va_end(ap);
 }
