6b25c8b35446170ad4375c968e6c7472d9339401
Bryan <bryanmartel.po@gmial.com>
2025-12-02 17:43:42 +0000

    fix Betty style code

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,18 +9,18 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    dlistint_t *current = head;
+	unsigned int i = 0;
+	dlistint_t *current = head;
 
-    while (current != NULL)
-    {
-        if (i == index)
-        {
-            return current;
-        }
-        current = current->next;
-        i++;
-    }
+	while (current != NULL)
+	{
+		if (i == index)
+		{
+			return current;
+		}
+		current = current->next;
+		i++;
+	}
 
-    return NULL;
+	return NULL;
 }
