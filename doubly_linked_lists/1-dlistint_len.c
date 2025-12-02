ff84ce0f71df3e6a8be88df57216135736536b7b
Bryan <bryanmartel.po@gmial.com>
2025-12-02 09:51:08 +0000

    fix betty style documentation

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Counts the number of elements in a dlistint_t list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
