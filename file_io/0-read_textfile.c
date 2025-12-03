a3ddd48ee40aa6bee83dc384bf6ef56b97e93da6
Bryan <bryanmartel.po@gmial.com>
2025-12-03 11:45:47 +0000

    fix Betty coding style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
  *
- * Return: The actual number of letters read and printed, or 0 on failure.
+ * Return: number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	ssize_t r, w;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
@@ -20,30 +20,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
+	r = read(fd, buf, letters);
+	if (r == -1)
 	{
-		free(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1 || w != r)
 	{
-		free(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
 
-	free(buffer);
+	free(buf);
 	close(fd);
-	return (bytes_written);
+
+	return (w);
 }
